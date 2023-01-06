f3517f90f9ece107d847f9e211c3cd45bd9d471c
hitansi <hitansiv.tagline@gmail.com>
2023-01-06 15:52:55 +0530

    create task4 using collections module.

--- task1.py
+++ task1.py
@@ -1,3 +1,4 @@
+from collections import Counter
 '''
 1. Create a list of 10 numbers and show the 5 different operations options to the user on screen. The 5 different operations are as following,
  A. Length of the list
@@ -17,23 +18,30 @@ o List without duplicate numbers: [2, 3, 4, 12, 44, 1, 3]
 
 '''
 
+#length of the list
 numbers = [2, 4, 5, 2, 12, 44, 5, 1, 2, 3]
-print(len(numbers))
+print("length of the list is :",len(numbers))
 
+#display first three numbers
 print(numbers[:3])
 
+#display sum off odd number
 sum = 0 
 for i in numbers:
     if i%2 !=0:
         sum += i
-print(sum)
+print("Sum of odd number is",sum)
 
-my_lst = [i  for i in numbers if numbers.count(i)>1]
-set1 = list(set(my_lst))
-print(len(set1))
+#display number of duplicate number
+counts = dict(Counter(numbers))
+duplicate = {key:value for key,value in counts.items() if value > 1}
+print(len(duplicate))
 
+#display list without duplicate number
 lst1 = []
 for i in numbers:
     if i not in lst1:
         lst1.append(i)
 print(lst1)
+
+
