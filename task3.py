ef7064ff27e34e0b24af1ae28d8c76a394201325
hitansi <hitansiv.tagline@gmail.com>
2023-01-10 15:54:50 +0530

    find word tree

--- task3.py
+++ task3.py
@@ -1,3 +1,4 @@
+from collections import Counter
 '''
 You are given a list of sentences. Create a word tree from each sentence and find how many times the word is appeared in other word trees.
  Input:
@@ -6,3 +7,13 @@ sentences = [“My name is Ram”, “”He is a good person”, “You should b
 word_trees = [[“My”, “name”, “is”, “Ram”], [“He”, “is”, “a”, “good”, “person”], [“You”, “should”, “be”, “careful”, “while”, “coding”], [“He”, “can”, “do”, “better”], [“The”, “person”, “is”, “mysterious”], [“Jay”, “Shree”, “Ram”], [“It”, “is”, “my”, “pen”]]
 Number of time each word appears:
 {“My”: 1, “name”: 1, “is”: 4, “Ram”: 2, “He”: 2, “good”: 1, “person”: 2, “You”: 1 ......} Likewise all word should be covered.'''
+
+
+sentences = ["My name is Ram", "He is a good person", "You should be careful while coding", "He can do better", "The person is mysterious", "Jay Shree Ram", "It is my pen."]
+
+#word_trees
+word_trees = []
+for i in sentences:
+    word_trees.append(i.split())
+print("word_trees =",word_trees)
+
