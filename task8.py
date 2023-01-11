e81901b3283f1fb3b3d1ca94b78d44f85d9ee151
hitansi <hitansiv.tagline@gmail.com>
2023-01-11 18:23:40 +0530

    Sort list without in-built function

--- task8.py
+++ task8.py
@@ -5,3 +5,13 @@ numbers = [2, 5, 6, 1, 3, 8, 9, 10]
  Output:
 [1, 2, 3, 5, 6, 8, 9, 10]
 '''
+
+#sort list without in-built function
+numbers = [2, 5, 6, 1, 3, 8, 9, 10]
+for i in range (len(numbers)):
+    for j in range(len(numbers)):
+        if(numbers[i] < numbers[j]):
+            temp = numbers[i]
+            numbers[i] = numbers[j]
+            numbers[j] = temp
+print("sorted list ::",numbers)
