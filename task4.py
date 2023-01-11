2e35cbb6d6ab8393e5a89b7eba64a7fe45a9ca9e
hitansi <hitansiv.tagline@gmail.com>
2023-01-11 10:00:01 +0530

    create recursion for find nth number of fibonacci

--- task4.py
+++ task4.py
@@ -9,3 +9,15 @@ o Also, check if your recursive function is able to return the Fibonacci value a
 numbers = [23, 44, 5, 67, 1, 1, 2, 4, 5]
 o Output: 152
 '''
+
+'''Create a recursive function which returns the n-th Fibonacci number. [Fibonacci
+sequence: 1, 1, 2, 3, 5, 8, 13, 21, ...]'''
+
+input_num = int(input("Enter a valid number::"))
+memoized = {0: 0, 1: 1}
+def fibonacci(number):
+   if number in memoized: 
+       return memoized[number]
+   memoized[number] = fibonacci(number - 1) + fibonacci(number - 2)  
+   return memoized[number]
+print(fibonacci(input_num))
