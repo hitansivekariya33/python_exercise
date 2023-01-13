4e12b44d8c944a09925071816f4f6dab7c4d41b9
hitansi <hitansiv.tagline@gmail.com>
2023-01-13 10:38:34 +0530

    Done pattern 2

--- task10.py
+++ task10.py
@@ -19,3 +19,17 @@ for i in range(1, numbers+1):
     i = -i
   print("*" * i + " " * (numbers - i*2) + "*"*i)
 
+print("---------------------------------------------")
+#pattern2
+'''
+  *  
+ *** 
+*****
+ *** 
+  *  
+'''
+for i in range(1, numbers+1):
+  i = i - (numbers//2 +1)
+  if i < 0:
+    i = -i
+  print(" " * i + "*" * (numbers - i*2) + " "*i)
