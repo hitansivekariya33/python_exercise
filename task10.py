e547f9820133d07b511c52f36fda0809d8d9b3c9
hitansi <hitansiv.tagline@gmail.com>
2023-01-13 10:37:51 +0530

    Done pattern 1

--- task10.py
+++ task10.py
@@ -2,3 +2,20 @@
 Print the following patterns. In the patterns ‘_’ (underscore) denotes the space.
 '''
 
+numbers = int(input("Enter a number::"))
+
+print("---------------------------------------------")
+#pattern 1
+'''
+** **
+*   *
+
+*   *
+** **
+'''
+for i in range(1, numbers+1):
+  i = i - (numbers//2 +1)
+  if i < 0:
+    i = -i
+  print("*" * i + " " * (numbers - i*2) + "*"*i)
+
