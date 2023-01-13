71aff3a870597f203d2b17895d37b570412f3207
hitansi <hitansiv.tagline@gmail.com>
2023-01-13 10:40:24 +0530

    Done pattern 5

--- task10.py
+++ task10.py
@@ -67,3 +67,18 @@ for i in range(0, numbers):
         else:
             print(" ", end="")
     print()
+
+print("---------------------------------------------")
+#pattern5
+'''
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+'''
+num=1
+for i in range(1,numbers):
+    for j in range(1,i+1):
+        print(num , end=' ')
+        num+=1
+    print("")
