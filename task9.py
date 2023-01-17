1a0d8499ab1aa6928ffb786cead3d53d2fc9dcb5
hitansi <hitansiv.tagline@gmail.com>
2023-01-17 10:38:59 +0530

    find difference between two dates removing night intervals

--- task9.py
+++ task9.py
@@ -1,5 +1,13 @@
+from datetime import *
+
 '''
 Take two datetimes from user start_time and end_time. Calculates the difference between start_time and end_time by 
 removing night intervals (12 AM to 6 AM). (Hint: You can use python intervals module)
 '''
+
+startdate_str = datetime(2023,1,10,3, 00, 00)
+enddate_str = datetime(2023,1,15,3,00,00)
+diff_date = enddate_str - startdate_str
+intervals= timedelta(hours=6)*diff_date.days
+print(diff_date-intervals)
 
