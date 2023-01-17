23723a7f41869f1d841f9630fb3df547a715f71b
hitansi <hitansiv.tagline@gmail.com>
2023-01-17 14:16:10 +0530

    find intervals using portion module

--- task9.py
+++ task9.py
@@ -1,5 +1,6 @@
 from datetime import *
-
+import portion as p
+from collections import *
 '''
 Take two datetimes from user start_time and end_time. Calculates the difference between start_time and end_time by 
 removing night intervals (12 AM to 6 AM). (Hint: You can use python intervals module)
@@ -11,3 +12,12 @@ diff_date = enddate_str - startdate_str
 intervals= timedelta(hours=6)*diff_date.days
 print(diff_date-intervals)
 
+
+delta = enddate_str - startdate_str   # returns timedelta
+test_time = []
+for i in range(delta.days + 1):
+    test_time.append(p.closed(startdate_str + timedelta(i),startdate_str+timedelta(i,hours=6)))
+    withhours=startdate_str + timedelta(i)
+    withouthours=startdate_str+timedelta(i,hours=6)
+    diff = withouthours-withhours
+print(delta-delta.days*diff)
