36c85e72351425fbe6aa3dce3199d47705e66a9f
hitansi <hitansiv.tagline@gmail.com>
2023-01-19 16:12:37 +0530

    resolve task9

--- task9.py
+++ task9.py
@@ -6,28 +6,37 @@ Take two datetimes from user start_time and end_time. Calculates the difference 
 removing night intervals (12 AM to 6 AM). (Hint: You can use python intervals module)
 '''
 
-startdate_str = datetime(2023,1,10,3, 00, 00)
-enddate_str = datetime(2023,1,15,3,00,00)
-diff_date = enddate_str - startdate_str
-intervals= timedelta(hours=6)*diff_date.days
-print(diff_date-intervals)
+start_date = datetime(2023,1,10,3, 00, 00)
+end_date = datetime(2023,1,15,3,00,00)
+diff_date = end_date - start_date
+intervals = timedelta(hours = 6) * diff_date.days
+print(diff_date - intervals)
 
 
-#taked input of two date and time
+#take two datetimes input
 start_date_time = input('Enter start time DD-MM-YYYY H:M:S format:~ ')
 end_date_time = input('Enter end time DD-MM-YYYY H:M:S in 24 hours format:~ ')
 
 start_date_time = datetime.strptime(start_date_time,'%d-%m-%Y %H:%M:%S')
 end_date_time = datetime.strptime(end_date_time,'%d-%m-%Y %H:%M:%S')
 
-#find differance between two dates
-date_differance =  end_date_time-start_date_time
+#find difference between two dates
+date_difference = end_date_time - start_date_time
 
 #find interval between start date and end date
 start_end_interval = p.closed(start_date_time,end_date_time)
 
 #find night interval
-night_time = []
+night_times = []
 while start_date_time <= end_date_time:
-    night_time.append(p.closed(datetime.combine(start_date_time,time()),datetime.combine(start_date_time,time())+timedelta(hours=6)))
-    start_date_time+=timedelta(days=1)
+    night_times.append(p.closed(datetime.combine(start_date_time,time()),datetime.combine(start_date_time,time()) + timedelta(hours=6)))
+    start_date_time += timedelta(days=1)
+
+night_intervals = [start_end_interval&nights for nights in night_times]
+remove_time = timedelta(0)
+
+for i in range(len(night_intervals)):
+    if not night_intervals[i].empty:
+        tmp = night_intervals[i].upper - night_intervals[i].lower
+        remove_time += tmp
+print('Total time between two dates without night intervals:~ ',date_difference - remove_time) 
